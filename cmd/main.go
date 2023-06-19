09ea6e5f3090bc1ee64b5fd6da3246b1438569b8
Chandan <chandancvp08@gmail.com>
2023-06-19 08:49:12 +0530

    converted it to json

--- cmd/main.go
+++ cmd/main.go
@@ -15,11 +15,13 @@ func main() {
 	db.MigrageDB()
 
 	r := gin.Default()
+
 	r.GET("/ping", handlers.Ping)
 	r.POST("/register", handlers.Register)
 	r.POST("/signup", handlers.SignUP)
 	r.POST("/login", handlers.Login)
 	r.GET("/logout", handlers.Logout)
 	r.GET("/validate", middlewares.ReqAuth, handlers.Validate)
+	r.Static("/docs", "./docs")
 	r.Run() // listen and serve on
 }
