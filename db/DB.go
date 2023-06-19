3870efcc6d20127d4873346fb96ca246ed27769b
Chandan <chandancvp08@gmail.com>
2023-06-19 08:39:09 +0530

    added docs

--- db/DB.go
+++ db/DB.go
@@ -1,9 +1,11 @@
 package db
 
 import (
+	"fmt"
 	"log"
 	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -11,7 +13,14 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	dsn := os.Getenv("DATABASE_URL")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	fmt.Println("here is the db:" + os.Getenv("DATABASE_URL"))
+	fmt.Println("here is the secret:" + os.Getenv("SECRET"))
+	var dsn = os.Getenv("DATABASE_URL")
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	DB = db
 	if err != nil {
