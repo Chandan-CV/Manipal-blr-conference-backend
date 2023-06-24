07b8ec3e5d91980a5c1ec463900789a895fd4c20
Chandan <chandancvp08@gmail.com>
2023-06-24 21:15:26 +0530

    added the slash in setcookie

--- handlers/logout.go
+++ handlers/logout.go
@@ -8,6 +8,6 @@ import (
 
 func Logout(c *gin.Context) {
 	c.SetSameSite(http.SameSiteLaxMode)
-	c.SetCookie("Authorization", "logged out :)", 10, "", "", true, true)
+	c.SetCookie("Authorization", "logged out :)", 10, "/", "", true, true)
 	c.JSON(http.StatusOK, gin.H{"message": "user logged in! and token sent as a cookie"})
 }
