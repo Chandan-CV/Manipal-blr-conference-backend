e6ba2154c14d87d28b21cfb106d8f2830d958a61
Chandan <chandancvp08@gmail.com>
2023-06-25 09:40:04 +0530

    changed samesite property to None mode in logout

--- handlers/logout.go
+++ handlers/logout.go
@@ -7,7 +7,7 @@ import (
 )
 
 func Logout(c *gin.Context) {
-	c.SetSameSite(http.SameSiteDefaultMode)
+	c.SetSameSite(http.SameSiteNoneMode)
 	c.SetCookie("Authorization", "logged out :)", 10, "/", "", true, true)
 	c.JSON(http.StatusOK, gin.H{"message": "user logged in! and token sent as a cookie"})
 }
